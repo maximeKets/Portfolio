552edf7ab887056b74d509a8565f112ee94b6229
maxime <maximekets80@gmail.com>
2024-06-01 11:31:59 +0200

    update about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -22,7 +22,7 @@ function About() {
                         }}
                     >
                         <h1 style={{fontSize: "2.1em", paddingBottom: "20px"}}>
-                            Know Who <strong className="purple">I'M</strong>
+                            MON <strong className="purple">PARCOURS</strong>
                         </h1>
                         <Aboutcard/>
                     </Col>
@@ -35,7 +35,7 @@ function About() {
                     </Col>
                 </Row>
                 <h1 className="project-heading">
-                    Languages de  <strong className="purple">Programmation</strong>
+                    Languages de <strong className="purple">Programmation</strong>
                 </h1>
 
                 <Techstack/>
@@ -49,7 +49,6 @@ function About() {
                     Les <strong className="purple">Frameworks</strong>
                 </h1>
                 <Framework/>
-
             </Container>
         </Container>
     );
--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,44 +1,54 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import { ImPointRight } from "react-icons/im";
+import {ImPointRight} from "react-icons/im";
 
 function AboutCard() {
-  return (
-    <Card className="quote-card-view">
-      <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />
-            I am currently employed as a software developer at Juspay.
-            <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
-            <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
-          </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Games
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Travelling
-            </li>
-          </ul>
+    return (
+        <Card className="quote-card-view">
+            <Card.Body>
+                <blockquote className="blockquote mb-0">
+                    <p style={{textAlign: "justify"}}>
+                        Après 18 mois de formation au
+                        <a className="no-underline" rel="noreferrer" target={"_blank"}
+                           href={"https://le-campus-numerique.fr/formations-developpeur2/#webetapplisalternance"}>
+                            <span className="purple"> "campus numérique In the Alps"</span>
+                        </a>, dont 12 en alternance chez
+                        <span className="purple"> Park4Night </span>
+                        en tant que
+                        <span className="purple"> développeur Full Stack</span>
+                        , j'ai acquis une maîtrise des principaux langages et frameworks associés.
+                        <br/>
+                        <br/>
+                        Depuis un an, je travaille en freelance,
+                        réalisant plusieurs projets principalement en Python.
+                        <br/>
+                        <br/>
+                        Aujourd'hui, mes forces sont :
+                    </p>
+                    <ul>
+                        <li className="about-activity">
+                            <ImPointRight/> Python, PHP, JavaScript
+                        </li>
+                        <li className="about-activity">
+                            <ImPointRight/> Force de proposition
+                        </li>
+                        <li className="about-activity">
+                            <ImPointRight/> Résilience et résolution de problèmes
+                        </li>
+                        <li className="about-activity">
+                            <ImPointRight/> Fluent english
+                        </li>
+                    </ul>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
-        </blockquote>
-      </Card.Body>
-    </Card>
-  );
+                    <p style={{color: "rgb(155 126 172)"}}>
+                        TODO : change name
+                        "Strive to build things that make a difference!"{" "}
+                    </p>
+                    <footer className="blockquote-footer">Soumyajit</footer>
+                </blockquote>
+            </Card.Body>
+        </Card>
+    );
 }
 
 export default AboutCard;
