2b44959e2a88bf67304a988e7e9bea6f9839b198
maxime <maximekets80@gmail.com>
2024-06-02 13:42:30 +0200

    Add @vercel/speed-insights

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
@@ -6,42 +6,49 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {SpeedInsights} from '@vercel/speed-insights/react';
+
 
 function App() {
-  const [load, updateLoad] = useState(true);
+    const [load, updateLoad] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      updateLoad(false);
-    }, 1200);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            updateLoad(false);
+        }, 1200);
 
-    return () => clearTimeout(timer);
-  }, []);
+        return () => clearTimeout(timer);
+    }, []);
 
-  return (
-      <Router>
-        <Preloader load={load} />
-        <div className={`App min-h-screen flex flex-col ${load ? "no-scroll" : "scroll"}`}>
-          <Navbar />
-          <ScrollToTop />
-          <div className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/projets" element={<Projects />} />
-              <Route path="/profil" element={<About />} />
-              <Route path="/cv" element={<Resume />} />
-              <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
-          </div>
-          <Footer />
+    return (
+        <div>
+            <Router>
+                <Preloader load={load}/>
+                <div className={`App min-h-screen flex flex-col ${load ? "no-scroll" : "scroll"}`}>
+                    <Navbar/>
+                    <ScrollToTop/>
+                    <div className="flex-grow">
+                        <Routes>
+                            <Route path="/" element={<Home/>}/>
+                            <Route path="/projets" element={<Projects/>}/>
+                            <Route path="/profil" element={<About/>}/>
+                            <Route path="/cv" element={<Resume/>}/>
+                            <Route path="*" element={<Navigate to="/"/>}/>
+                        </Routes>
+                        <SpeedInsights/>
+                    </div>
+                    <Footer/>
+                </div>
+            </Router>
+            <SpeedInsights/>
         </div>
-      </Router>
-  );
+    )
+        ;
 }
 
 export default App;
