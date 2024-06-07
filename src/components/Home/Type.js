1fa93323a33bca9095baa6b18874723fcc70ae71
maxime <maximekets80@gmail.com>
2024-06-07 11:37:55 +0200

    update Projects.js increase speed typewritter

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -2,21 +2,22 @@ import React from "react";
 import Typewriter from "typewriter-effect";
 
 function Type() {
-  return (
-    <Typewriter
-      options={{
-        strings: [
-          "Développeur Web",
-          "Futur Développeur IA / Data analyst",
-          "EPSI Montpellier Bachelor Développeur IA ",
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 100,
+    return (
+        <Typewriter
+            options={{
+                strings: [
+                    "Développeur Web",
+                    "Futur Développeur IA / Data analyst",
+                    "EPSI Montpellier Bachelor Développeur IA ",
+                ],
+                autoStart: true,
+                loop: true,
+                typeSpeed: 200,
+                deleteSpeed: 100,
 
-      }}
-    />
-  );
+            }}
+        />
+    );
 }
 
 export default Type;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -13,10 +13,10 @@ function Projects() {
             <Particle/>
             <Container>
                 <h1 className="project-heading">
-                    My Recent <strong className="purple">Works </strong>
+                     <strong className="purple">Projets </strong>récents
                 </h1>
                 <p style={{color: "white"}}>
-                    Here are a few projects I've worked on recently.
+                    Quelques projets pertinents.
                 </p>
                 <Row style={{justifyContent: "center", paddingBottom: "10px"}}>
                     <Col md={4} className="project-card">
